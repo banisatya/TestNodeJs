b06f6b0213df8a4c87be76a619f9047208e113ea
bani <bani.satya@gmail.com>
2021-07-31 21:57:25 +0530

    Adding the run bat file

---
+++ run.bat
@@ -0,0 +1,3 @@
+echo "copying files to destination"
+xcopy "C:\WINDOWS\system32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\my node project" "F:\Project Practice\JenkinsNodeJs" /s /y
+echo "File copy is completed...about to start nodejs"
